8483ab1b5e2b8e4809cc03c3a57e5596c49ba25d
ORibn-dev <63300748+ORibn-dev@users.noreply.github.com>
2021-06-07 02:17:45 +0300

    Update1

--- ShearingShrubs/Assets/ShearingShrubs/Scripts/MovingChainsaw.cs
+++ ShearingShrubs/Assets/ShearingShrubs/Scripts/MovingChainsaw.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovingChainsaw : MonoBehaviour
 {
+    [Header("Chainsaw movement borders")]
     [SerializeField] private float borderX1;
     [SerializeField] private float borderX2;
     [SerializeField] private float borderY1;
     [SerializeField] private float borderY2;
+    [Space]
     [SerializeField] private float speed;
 
     private Touch touch;
